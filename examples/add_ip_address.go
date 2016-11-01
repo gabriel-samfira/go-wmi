4e1203cad7cc77c88ceb2d9a9a00e04b47b4aa7d
Gabriel Adrian Samfira <samfiragabriel@gmail.com>
2016-11-01 08:02:05 -0700

    Added simple ip address examples

---
+++ examples/add_ip_address.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gabriel-samfira/go-wmi/wmi"
+)
+
+
+func main() {
+	w, err := wmi.NewConnection(".", `Root\StandardCimv2`, nil, nil, nil, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// See documentation on MSFT_NetIPAddress class at: https://msdn.microsoft.com/en-us/library/hh872425(v=vs.85).aspx
+	netip, err := w.Get("MSFT_NetIPAddress")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Create() method documentation at: https://msdn.microsoft.com/en-us/library/hh872254(v=vs.85).aspx
+	_, err = netip.Get("Create", nil, "Ethernet1", "10.10.10.11", 2, 24)
+	if err != nil {
+		fmt.Printf("Error running Create: %v", err)
+		os.Exit(1)
+	}
+	fmt.Println("Success!")
+	return
+}
---
+++ examples/get_ip_addresses.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gabriel-samfira/go-wmi/wmi"
+)
+
+
+func main() {
+	w, err := wmi.NewConnection(".", `Root\StandardCimv2`, nil, nil, nil, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// AddressFamily:
+	// 	2 - IPv4
+	//  23 - IPv6
+	qParams := []wmi.WMIQuery{
+		&wmi.WMIAndQuery{wmi.QueryFields{Key: "AddressFamily", Value: 2, Type: wmi.Equals}},
+	}
+	// See documentation on MSFT_NetIPAddress class at: https://msdn.microsoft.com/en-us/library/hh872425(v=vs.85).aspx
+	netip, err := w.Gwmi("MSFT_NetIPAddress", []string{}, qParams)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	elements, err := netip.Elements()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	if len(elements) > 0 {
+		for i := 0; i < len(elements); i++ {
+			address, err := elements[i].GetProperty("IPAddress")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			iface, err := elements[i].GetProperty("InterfaceAlias")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			fmt.Printf("Found IP %v on interface %v\n", address.Value(), iface.Value())
+		}
+	}
+	return
+}
